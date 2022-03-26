a0b2bfad95f824b796e847a7ae842fcc71bd2c6b
Darrian Bagley <dbagle14@gmail.com>
2022-03-26 09:44:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        Object.assign(this, { salad, soup, entree, dessert });
+    }
+}
+
+console.log(new Dinner('a','b','c','d'));
